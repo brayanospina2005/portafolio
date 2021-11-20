f2a9e9baaa1d05ed586cb3a5d3a4361ad393ebbb
brayanospina <ospinabrayan431@gmail.com>
2021-11-20 05:39:40 -0500

    se termina el proyecto despues de perderlo casi todo

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+function change() {
+    this.style.backgroundcolor = 'aqua';
+    
+}
+
+function mouseover() {
+    document.getElementById('mouseover').addEventListener('mouseover', change);
+        
+    }
+    
